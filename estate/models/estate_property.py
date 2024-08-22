c2bc76547e2bdf2a5c2d5c92948c64830a3746ea
andry <gorpynchenko79@gmail.com>
2024-08-22 22:32:46 +0300

    create views in estate_property_views.xml & menu in estate_menus.xml & add default fields, copy readonly in estate_property.py

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from odoo import fields, models
 
 class EstateProperty(models.Model):
@@ -7,18 +9,26 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=datetime.today() + timedelta(days=90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    active = fields.Boolean(default=True)
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
         ('east', 'East'),
         ('west', 'West'),
     ])
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled'),
+    ], required=True, copy=False, default='new')
